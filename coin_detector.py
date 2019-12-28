66137b26b2bd17f568c35503630a8ae5a96f6e50
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2019-12-29 00:45:11 +0330

    changed coin detection algortighm

--- coin_detector.py
+++ coin_detector.py
@@ -10,29 +10,36 @@ def find_coin(image_path):
 
     # Blur using 3 * 3 kernel.
     gray_blurred = cv2.blur(img, (7, 7))
+    rendered_image_path = 'rendered_image.png'
 
     # Apply Hough transform on the blurred image.
-    detected_circles = cv2.HoughCircles(gray_blurred,
-                                        cv2.HOUGH_GRADIENT, 1, 20, param1=100,
-                                        param2=30, minRadius=20, maxRadius=500)
+    for param2 in range(20, 100, 10):
+        detected_circles = cv2.HoughCircles(gray_blurred,
+                                            cv2.HOUGH_GRADIENT, 1, 20, param1=100,
+                                            param2=param2, minRadius=20 , maxRadius=500)
 
-    # Draw circles that are detected.
-    if detected_circles is not None:
+        # Draw circles that are detected.
+        if detected_circles is not None:
 
-        # Convert the circle parameters a, b and r to integers.
-        detected_circles = np.uint16(np.around(detected_circles))
-        colored = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-        for pt in detected_circles[0, :]:
-            a, b, r = pt[0], pt[1], pt[2]
+            # Convert the circle parameters a, b and r to integers.
+            detected_circles = np.uint16(np.around(detected_circles))
+            colored = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            for pt in detected_circles[0, :]:
 
-            # Draw the circumference of the circle.
-            cv2.circle(colored, (a, b), r, (0, 255, 0), 10)
+                a, b, r = pt[0], pt[1], pt[2]
 
-            # Draw a small circle (of radius 1) to show the center.
-            cv2.circle(colored, (a, b), 1, (0, 0, 255), 3)
-            # cv2.imshow("Detected Circle", img)
-            # cv2.waitKey(0)
+                # Draw the circumference of the circle.
+                cv2.circle(colored, (a, b), r, (0, 255, 0), 10)
 
-    rendered_image_path = 'rendered_image.png'
+                # Draw a small circle (of radius 1) to show the center.
+                cv2.circle(colored, (a, b), 1, (0, 0, 255), 3)
+                # cv2.imshow("Detected Circle", img)
+                # cv2.waitKey(0)
+                break
+
+        else:
+            break
+
+
     cv2.imwrite(rendered_image_path, colored)
     return rendered_image_path
